d5ae0a4950e49591f7bde43e24efbbe8ed47542d
Camoraz <racamo2006@gmail.com>
2024-11-16 17:46:00 +0100

    Statistics

--- function.py
+++ function.py
@@ -6,14 +6,13 @@ data_path = "AEDChallenge/data/datathon_participants.json"
 participants_pre = load_participants(data_path)
 
 categorical = {
-            "experience_level":1,
-            "preferred_role":-1,
+            "preferred_role":-2,
             "interest_in_challenges":5,
             "preferred_languages":5,
-            "availability":5
+            "availability":10
             }
 
-numeric = {"age":1, "year_of_study":1, "hackathons_done":1}
+numeric = {"experience_level":1, "age":1, "year_of_study":1, "hackathons_done":0.5}
 nlp = {"interests":1, "objective":5, }
 
 def process_data(particips):
@@ -26,10 +25,17 @@ def process_data(particips):
     "PhD": 6
     }
 
+    experience_map = {
+    "Beginner": 1,
+    "Intermediate": 2,
+    "Advanced": 3
+    }
+
     returning = particips
 
     for participant in returning:
         participant.year_of_study = year_mapping.get(participant.year_of_study, 0)  # Default to 0 if not found
+        participant.experience_level = experience_map.get(participant.experience_level, 0)  # Default to 0 if not found
     return returning
 
 def recommend(participant, n):
@@ -41,6 +47,7 @@ def recommend(participant, n):
     norm = sum(categorical.values()) + sum(numeric.values()) + sum(nlp.values())
 
     distance_list = [0]*len(participants)
+    effect_list = {}
 
     # Loop through all participants
     for i in range(len(participants)):
@@ -50,19 +57,29 @@ def recommend(participant, n):
 
         # Numeric - Euclidean Distance
         for variable, weight in numeric.items():
-            distance += abs(float(getattr(participant, variable))
+            change = abs(float(getattr(participant, variable))
                             - float(getattr(participants[i], variable))) * weight * norm
+            distance += change
+            effect_list[variable] = effect_list.get(variable, 0) + change
         
         # Categorical - Jaccard
         for variable, weight in categorical.items():
-            distance += int(getattr(participant, variable)
+            change = int(getattr(participant, variable)
                             != getattr(participants[i], variable)) * weight * norm
+            distance += change
+            effect_list[variable] = effect_list.get(variable, 0) + change
         
         distance_list[i] = distance
+
+    total_importance = sum(effect_list.values())
+
+    for key in effect_list:
+        effect_list[key] /= total_importance
 
     if n <= 0:
         return []
     
-    return distance_list[:n]
+    return distance_list[:n], effect_list
 
-print(recommend(participants_pre[15], 10))
+distance, effect = recommend(participants_pre[15], 10)
+print(effect)
