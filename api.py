20d2d73b904d268d49b98c3952c175a0aa7889e6
discomanfulanito <pablonecpas@gmail.com>
2024-11-16 23:12:22 +0100

    guardo por si la cago luego

--- api.py
+++ api.py
@@ -1,14 +1,22 @@
-@app.route("/add", methods=["POST"], strict_slashes=False)
-def add_articles():
-    title = request.json['title']
-    body = request.json['body']
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from format_data import FormData
 
-    article = Articles(
-        title=title,
-        body=body
-        )
 
-    db.session.add(article)
-    db.session.commit()
+app = Flask(__name__)
+CORS(app, origins=["http://localhost:5173"])  # Permite solo desde React
 
-    return article_schema.jsonify(article)
+@app.route('/api', methods=['POST'])
+def api_endpoint():
+    data = request.get_json()
+    
+    response = FormData(data['years'], data['role'], data['challenge'], data['lang'], data['level'], data['age'], data['hackathons'])
+
+    
+
+    response = f'years: {data['years']}, role: {data['role']}, challenge: {data['challenge']}, lang: {data['lang']}, level: {data['level']}, age: {data['age']}, hackathons: {data['hackathons']}'
+
+    return jsonify({"message": "Received", "data": response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import APIService from '../APIservice'
 import './App.css'
 
 function App() {
-  const [data, setData] = useState({'name':'Raul', 'age':'legal', 'year':3})
+  const [data, setData] = useState({'years':'', 'role':'', 'challenge':'', 'lang': '', 'availability':'', 'level':'', 'age':'', 'hackathons':''})
   const [message, setMessage] = useState('')
 
   const sendData = () => {
     APIService.getData(data)
     .then((response) => setMessage(response))
+  }
+
+  const handleChange = (event) => {
+    if (event.target.type == 'radio') {
+      console.log(event.target.value == 'radio', event.target.type)
+    }
+
+    const { name, value } = event.target
+    setData({...data, [name]: value})
   }
 
   const handleSubmit = (event) => {
@@ -17,11 +26,57 @@ function App() {
   }
   return (
     <>
-      <h1>Perfect Groups Generator AI powered blockchain ML</h1>
+      <h1>Perfect Groups Generator AI powered blockchain ML </h1>
       <button onClick={handleSubmit}>Click to return data</button>
       <form onSubmit={handleSubmit}>
         <label htmlFor=""></label>
-        <input type="" />
+        <p>{" ".concat( data['name'], '   ' ,data['years'], '   ' ,data['role'], '   ' ,data['challenge'], '   ' ,data['lang'], '   ' ,data['availability'], '   ' ,data['level'], '   ' ,data['age'], '   ' ,data['hackathons'])}</p>
+        <input placeholder='Name' name='name' value={data.name} onChange={handleChange} />
+        <div className='inputDiv'>
+          <label htmlFor="">Year of study: </label>
+          <input type='radio' name='years' value={'1'} onChange={handleChange}/>
+          <input type="radio" name="years" value={'2'} onChange={handleChange} />
+          <input type="radio" name="years" value={'3'} onChange={handleChange} />
+          <input type="radio" name="years" value={'4'} onChange={handleChange} />
+          <input type="radio" name="years" value={'masters'} onChange={handleChange}/>
+          <input type="radio" name="years" value={'phd'} onChange={handleChange} />
+        </div>
+        <div className='inputDiv'>
+          <label>Preferred Role: </label>
+          <input type='radio' name='role' value={'analysis'} onChange={handleChange}/>
+          <input type='radio' name='role' value={'visualization'} onChange={handleChange}/>
+          <input type='radio' name='role' value={'development'} onChange={handleChange}/>
+          <input type="radio" name="role" value={'design'} onChange={handleChange}/>
+        </div>
+        <div className='inputDiv'>
+          <label>Challenge selected: </label>
+          <input type="radio" name="challenge" value={'restb.ai'} onChange={handleChange} />
+          <input type="radio" name="challenge" value={'aed challenge'} onChange={handleChange}/>
+          <input type="radio" name="challenge" value={'mango challenge'} onChange={handleChange}/>
+        </div>
+        <div className='inputDiv'>
+          <label>Preferred Languages:  </label>
+          <input placeholder='Spanish, English, Catalan' name='lang' value={data.lang} onChange={handleChange}/>
+        </div>
+        <div className='inputDiv'>
+          <label>Availability:  </label>
+          <input placeholder='idk, esto se cambia' name='availability' value={data.availability} onChange={handleChange}/>
+        </div>
+        <div className='inputDiv'>
+          <label>Experience level:  </label>
+          <input type="radio" name="level" value={'beginner'}  onChange={handleChange}/>
+          <input type="radio" name="level" value={'intermediate'}  onChange={handleChange} />
+          <input type="radio" name="level" value={'advanced'}  onChange={handleChange} />
+        </div>
+        <div className='inputDiv'>
+          <label>Age:  </label>
+          <input placeholder='Your age' name='age' value={data.age} onChange={handleChange}/>
+        </div>
+        <div className='inputDiv'>
+          <label>N. of hackathons done</label>
+          <input placeholder='1, 2, ...' name='hackathons' value={data.hackathons} onChange={handleChange}/>
+        </div>
+        <button type='submit'>Submit</button>
       </form>
       {message && <p>{message}</p> }
     </>
